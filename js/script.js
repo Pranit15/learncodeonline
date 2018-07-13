d57bc78f65ebc4e8f82c39d78b922ec001584992
Pranit Batra <pranit.batra2@gmail.com>
2018-07-13 21:17:01 +0530

    Added index.html,css,js files and images related to it

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let question = document.querySelector('#question');
+let language = document.querySelector('#language');
+
+question.onchange = () => {
+    for (let i = 0; i < 5; i++) {
+        
+    }
+}
